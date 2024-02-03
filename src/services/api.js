5cc5844227552747a338356890dfe241319801c9
yurimoura <yurimoura9626@outlook.com>
2024-02-03 19:53:56 -0300

    funcionalidades do front entregues

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //https://crowded-codfish.cyclic.app
 export default axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://famous-newt-yoke.cyclic.app',
   timeout: 2000,
   headers: { "Content-Type": "application/json" },
 });
