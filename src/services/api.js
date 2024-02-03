fecad44238dbffbd098f268b9b014042ff977415
yurimoura <yurimoura9626@outlook.com>
2024-02-03 20:11:00 -0300

    correcao de url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //https://crowded-codfish.cyclic.app
 export default axios.create({
-  baseURL: 'https://famous-newt-yoke.cyclic.app',
+  baseURL: 'https://happy-dress-lamb.cyclic.app',
   timeout: 2000,
   headers: { "Content-Type": "application/json" },
 });
